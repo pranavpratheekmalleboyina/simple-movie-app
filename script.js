eec3828705a5f70beeef9894f08f689153134432
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-05-23 01:37:34 -0500

    Added the movie image as well as the path constants

---
+++ script.js
@@ -0,0 +1,3 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=21bcc4204a7004668a4b260daa25a534&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=21bcc4204a7004668a4b260daa25a534&query=";
